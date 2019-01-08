baa52ca50ce6fc300792b9abfc5285ddf7e96a86
mossnodie <moss@nodie.ink>
2019-01-09 01:18:18 +0800

    C到文件函数 C to file function

--- src/dao/tofile.c
+++ src/dao/tofile.c
@@ -1,97 +1,70 @@
-//
-// Created by moss on 2019/1/8.
-//
+/**
+ * @description  纯C语言实现的字符串 C language file function
+ * @copyright    2019 moss@nodie.ink
+ * @version      1.0.0
+ * @author       莫斯莫斯 mossnodie
+ * @file         tofile.c
+ * @date         2019-01-06
+ */
 
 #include "tofile.h"
 
-
-extern STATUS _schools_to_file(list *schools) {
-
+STATUS _schools_to_file(list *schools) {
     FILE *_p_f = NULL;
-
     _p_f = fopen("./sources/schools.dat", "w+");
-
     fprintf(_p_f, "\n");
     list_node *ln = schools->head;
     while (ln) {
-
         fprintf(_p_f, "%ld\n", ((school *) ln->data)->id);
         fprintf(_p_f, "%s\n", ((school *) ln->data)->name.ch);
         fprintf(_p_f, "%d\n", ((school *) ln->data)->score);
         fprintf(_p_f, "%d\n", ((school *) ln->data)->man_score);
         fprintf(_p_f, "%d\n", ((school *) ln->data)->woman_score);
-
         ln = ln->next;
 
     }
-
-
     fclose(_p_f);
-
 }
 
-extern STATUS _projects_to_file(list *projects) {
-
+STATUS _projects_to_file(list *projects) {
     FILE *_p_f = NULL;
-
     _p_f = fopen("./sources/projects.dat", "w+");
     fprintf(_p_f, "\n");
-
     list_node *ln = projects->head;
     while (ln) {
-
         fprintf(_p_f, "%ld\n", ((project *) ln->data)->id);
         fprintf(_p_f, "%s\n", ((project *) ln->data)->name.ch);
         fprintf(_p_f, "%d\n", ((project *) ln->data)->type);
-
         ln = ln->next;
-
     }
-
-
     fclose(_p_f);
-
-
 }
 
-extern STATUS _contacts_to_file(list *contacts) {
-
+STATUS _contacts_to_file(list *contacts) {
     FILE *_p_f = NULL;
-
     _p_f = fopen("./sources/contacts.dat", "w+");
     fprintf(_p_f, "\n");
-
     list_node *ln = contacts->head;
     while (ln) {
-
         fprintf(_p_f, "%ld\n", ((contact *) ln->data)->pid);
         fprintf(_p_f, "%ld\n", ((contact *) ln->data)->sid);
         fprintf(_p_f, "%d\n", ((contact *) ln->data)->score);
-
         ln = ln->next;
-
     }
-
-
     fclose(_p_f);
 
 }
 
 
-extern STATUS _file_to_schools(list *schools) {
+STATUS _file_to_schools(list *schools) {
     FILE *_p_f = NULL;
-
     _p_f = fopen("./sources/schools.dat", "r+");
     if (!_p_f) return ERROR;
-
     char *buff = (malloc(sizeof(char) * 100));
-
-
     fseek(_p_f, 0L, SEEK_END);
     long end = ftell(_p_f);
     fseek(_p_f, 0L, SEEK_SET);
     long start = ftell(_p_f);
-
     fscanf(_p_f, "%s\n", buff);
     while (end != start) {
         school s;
@@ -109,25 +82,18 @@ extern STATUS _file_to_schools(list *schools) {
         school_get_id(s);
         start = ftell(_p_f);
     }
-
-
     fclose(_p_f);
 }
 
-extern STATUS _files_to_project(list *projects) {
+STATUS _files_to_project(list *projects) {
     FILE *_p_f = NULL;
-
     _p_f = fopen("./sources/projects.dat", "r+");
     if (!_p_f) return ERROR;
-
     char *buff = (malloc(sizeof(char) * 100));
-
-
     fseek(_p_f, 0L, SEEK_END);
     long end = ftell(_p_f);
     fseek(_p_f, 0L, SEEK_SET);
     long start = ftell(_p_f);
-
     fscanf(_p_f, "%s\n", buff);
     while (end != start) {
         project p;
@@ -141,33 +107,20 @@ extern STATUS _files_to_project(list *projects) {
         _list_head_push(projects, &p);
         start = ftell(_p_f);
     }
-
     free(buff);
-
     fclose(_p_f);
-
 }
 
-extern STATUS _file_to_contacts(list *contacts) {
-
+STATUS _file_to_contacts(list *contacts) {
     FILE *_p_f = NULL;
-/*
-    fprintf(_p_f,"%ld\n",((contact*)ln->data)->pid);
-    fprintf(_p_f,"%ld\n",((contact*)ln->data)->sid);
-    fprintf(_p_f,"%d\n",((contact*)ln->data)->score);
-*/
     _p_f = fopen("./sources/contacts.dat", "r+");
     if (!_p_f) return ERROR;
-
     char *buff = (malloc(sizeof(char) * 100));
-
-
     fseek(_p_f, 0L, SEEK_END);
     long end = ftell(_p_f);
     fseek(_p_f, 0L, SEEK_SET);
     long start = ftell(_p_f);
     fscanf(_p_f, "%s\n", buff);
-
     while (end != start) {
         contact c;
         fscanf(_p_f, "%s\n", buff);
@@ -179,10 +132,6 @@ extern STATUS _file_to_contacts(list *contacts) {
         _list_head_push(contacts, &c);
         start = ftell(_p_f);
     }
-
-
     free(buff);
-
     fclose(_p_f);
-
 }
--- src/dao/tofile.h
+++ src/dao/tofile.h
@@ -1,19 +1,43 @@
-//
-// Created by moss on 2019/1/8.
-//
+/**
+ * @description  纯C语言实现的字符串 C language file function
+ * @copyright    2019 moss@nodie.ink
+ * @version      1.0.0
+ * @author       莫斯莫斯 mossnodie
+ * @file         tofile.h
+ * @date         2019-01-06
+ */
 
 #ifndef GAMES_SCORE_STATISTICS_TOFILE_H
 #define GAMES_SCORE_STATISTICS_TOFILE_H
+
 #include "../datastructs/list.h"
 #include "../../lib/stdhead.h"
 #include "../objects/school.h"
 #include "../objects/project.h"
 #include "../objects/contact.h"
-extern STATUS _schools_to_file(list* schools);
-extern STATUS _projects_to_file(list* projects);
-extern STATUS _contacts_to_file(list * contacts);
 
-extern STATUS _file_to_schools(list* schools);
-extern STATUS _files_to_project(list* projects);
-extern STATUS _file_to_contacts(list * contacts);
+/**
+ * @description 数据结构到文件 Data structure to file
+ * @param       定义的三个类型 school project contact
+ * @return      OK ERROR OVERFLOW
+ * @others      第一行含有其他干扰故第一行避开 The file of first line contains other interference, so the first line avoids
+ */
+extern STATUS _schools_to_file(list *schools);
+
+extern STATUS _projects_to_file(list *projects);
+
+extern STATUS _contacts_to_file(list *contacts);
+
+/**
+ * @description 文件到数据结构 file to Data structure
+ * @param       定义的三个类型的数据结构 schools projects contacts
+ * @return      OK ERROR OVERFLOW
+ * @others      第一行含有其他干扰故第一行避开 The file of first line contains other interference, so the first line avoids
+ */
+extern STATUS _file_to_schools(list *schools);
+
+extern STATUS _files_to_project(list *projects);
+
+extern STATUS _file_to_contacts(list *contacts);
+
 #endif //GAMES_SCORE_STATISTICS_TOFILE_H
